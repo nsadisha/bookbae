dee22ce6bfb48e6dfa0516e7f10c9f109919cd6d
nsadisha <nsadisha@gmail.com>
2021-09-14 20:43:52 +0530

    bootstrap css moved to main externel css file

--- register.php
+++ register.php
@@ -7,11 +7,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap css -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- custom css -->
     <link rel="stylesheet" href="css/styles.css">
-    <title>Register</title>
+    <title>BookBae | Register</title>
 </head>
 <body>
 
