67b6c3aaf15d92a27c3ea874f1b15df956c82f8c
nsadisha <nsadisha@gmail.com>
2021-09-14 21:06:45 +0530

    main routes created

---
+++ signin.php
@@ -0,0 +1,30 @@
+<?php include "components/footer.php" ?>
+<?php include "components/navbar.php" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- custom css -->
+    <link rel="stylesheet" href="css/styles.css">
+    <title>BookBae | Sign in</title>
+</head>
+<body>
+
+    <!-- Navbar starts -->
+    <?php navbar("signin"); ?>
+    <!-- Navbar ends -->
+
+    <!-- Page content starts -->
+    <h1 class="text-center">Signin</h1>
+    <!-- Page content ends -->
+
+    <!-- Footer starts -->
+    <?php footer(); ?>
+    <!-- Footer ends -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
