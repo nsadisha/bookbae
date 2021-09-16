187101e1a54e4dcf5fbd4b557a9f12d99353a41a
tchamalsha <tharu.chamalsha@gmail.com>
2021-09-16 18:32:42 +0530

    make 3d slider

--- view.php
+++ view.php
@@ -10,46 +10,45 @@
     <!-- custom css -->
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/view.css">
+
+    <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <!-- swiper css -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
     <title>BookBae | Register</title>
 </head>
 <body>
 
     <!-- Navbar starts -->
-    <?php navbar("register"); ?>
+  
     <!-- Navbar ends -->
 
     <!-- Page content starts -->
     <div class="container">
         <h1 class="text-center">Rotten school and great smelling bee</h1>
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <div class="swiper myswiper">
+            <div class="swiper-wrapper" style="height: fit-content;">
+                <div class="card swiper-slide" style="width: 18rem;">
+                    <img src="assets/images/view page/1.jpg" class="card-img-top" alt="...">
+                </div>
+                <div class="card swiper-slide" style="width: 18rem;">
+                    <img src="assets/images/view page/1.jpg" class="card-img-top" alt="...">
+                </div>
+                <div class="card swiper-slide" style="width: 18rem;">
+                    <img src="assets/images/view page/1.jpg" class="card-img-top" alt="...">
+                </div>
+                <div class="card swiper-slide" style="width: 18rem;">
+                    <img src="assets/images/view page/1.jpg" class="card-img-top" alt="...">
+                </div>
+            </div><br>
+            <div class="swiper-pagination"></div>
         </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-            <img src="..." class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-        </div>
-        <div class="container-fluid">
-            
+        <div class="container">
+            <table>
+                <tr>
+                    <th>ISBN</th>
+                    <td>12131</th>
+                </tr>
         </div>
 
     </div>
@@ -57,11 +56,14 @@
     <!-- Page content ends -->
 
     <!-- Footer starts -->
-    <?php footer(); ?>
+    
     <!-- Footer ends -->
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<script src="js/view.js"></script>
 </html>
