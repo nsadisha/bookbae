47822513de88db1f3b7c8becac40e9fce48bbe3d
tchamalsha <tharu.chamalsha@gmail.com>
2021-09-20 23:25:46 +0530

    all orders to profile

--- profile.php
+++ profile.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- custom css -->
-    <link rel="stylesheet" href="css/styles.css">
+   
     <link rel="stylesheet" href="css/profile.css">
 
     <!-- bootstrap -->
@@ -27,21 +27,45 @@
     <!-- Navbar starts -->
     <?php navbar(""); ?>
     <!-- Navbar ends -->
+    
+    <!-- Page content starts -->
     <div class="container-fluid header">
         <div class="row">
-            <div class="col-md-4 my-3 m-auto">
-                <div class="dp"><img src="assets\images\profile\dp.png"></div>
+            <div class="col-md-4  d-flex justify-content-center ">
+                <img src="assets\images\profile\dp.png" class="dp" style="width:12rem; height:12rem;">
             </div>
             <div class="col-md-8 m-auto">
                 <div class="heading"><p>Welcome Ada shelby </p></div>
                 <div class="sub-heading"><P id="sub-heading">Here's where you'll find all your account details as well as your shopping history</p></div>
             </div>
         </div>
-
     </div>
-    <!-- Page content starts -->
+    <div class="container-fluid components my-5">
+        <div class="row">
+            <div class="col-md-4 orders m-auto p-4">
+                <h4 class="text-center" style="font-weight:bold;">My orders</h4>
+                <p class="text-center" style="color:#495057;">Manage your orders and track your last order</p>
+                <h5 style="font-weight:bold;">My next delivery</h5>
+                <p style="color:#495057;">Tuesday 19th January 2021</p>
+                <div class="row align-items-center">
+                    <div class="col-sm-6 ">
+                        <button class="btn btn-primary mx-auto text-center" type="submit"><i class="bi bi-pin-map p-1"></i>track order</button>
+                    </div>
+                    <div class="col-sm-6">
+                        <a src="orders.php" class="link-to-orders">See all orders</a>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="col-md-4 justify-content-center orders m-auto">
+                <h4>My orders</h4>
+                <p>Manage your orders and track your last order</p>
+                <h5>My last order</h5>
+                <p>Manage your orders and track your last order</p>
+            </div>
+        </div>
+    </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
 <script src="js/profile.js"></script>
+</html>
