6cf357600e8ef886fc61c8f5d253faf7fbfcbda4
tchamalsha <tharu.chamalsha@gmail.com>
2021-09-22 01:22:43 +0530

    add verification pages

---
+++ verify.php
@@ -0,0 +1,42 @@
+<?php include "components/footer.php" ?>
+<?php include "components/navbar.php" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- custom css -->
+   
+    <link rel="stylesheet" href="css/verify.css">
+
+    <!-- bootstrap -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <!-- swiper css -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
+    <!--google fonts -->
+    
+    
+    <title>BookBae | Verify</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="assets\images\profile\undraw_verified_re_4io7.png">
+            </div>
+            <div class="col-md-6 justify-content-center">
+                <div class="row">
+                    <img src="assets\images\profile\undraw_Mail_sent_re_0ofv.png">
+                </div>
+                <div class="row">
+                    <p>Before creating account, we need toverify your email ID <strong>example@gmail.com</strong><p> 
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
