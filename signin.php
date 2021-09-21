af4130a12bf615312d635a461c084f5a88354e58
Vinojan Veerapathrathasan <48822560+Vinojan1999@users.noreply.github.com>
2021-09-21 20:46:27 +0530

    Sign in & Register form updated

--- signin.php
+++ signin.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- custom css -->
     <link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/signin.css">
     <link rel="stylesheet" href="css/responsive.css">	
     <title>BookBae | Sign in</title>
 </head>
@@ -19,60 +20,8 @@
     <!-- Navbar ends -->
 
     <!-- Page content starts -->
-    <h1 class="text-center">Account</h1>
-    <!-- //////////////////////////////// -->
-    <div class="form-main">
-			<!-- vertical tabs -->
-			<div class="vertical-tab">
-				<div id="section1" class="section">
-					<input type="radio" name="sections" id="option1" checked>
-					<label for="option1" class="form-icon"><span class="fa fa-user-circle" aria-hidden="true"></span>Login</label>
-					<article>
-						<form action="#" method="post">
-							<h3 class="form-head">Login Here</h3>
-							<div class="input">
-								<span class="fa fa-envelope-o" aria-hidden="true"></span>
-								<input type="email" placeholder="Email" name="email" required />
-							</div>
-							<div class="input">
-								<span class="fa fa-key" aria-hidden="true"></span>
-								<input type="password" placeholder="Password" name="password" required />
-							</div>
-							<button type="submit" class="btn submit">Login</button>
-							<a href="#" class="forgot-pwd">Forgot Password?</a>
-						</form>
-					</article>
-				</div>
-				<div id="section2" class="section">
-					<input type="radio" name="sections" id="option2">
-					<label for="option2" class="form-icon"><span class="fa fa-pencil-square" aria-hidden="true"></span>Register</label>
-					<article>
-						<form action="#" method="post">
-							<h3 class="form-head">Register Here</h3>
-							<div class="input">
-								<span class="fa fa-user-o" aria-hidden="true"></span>
-								<input type="text" placeholder="Username" name="name" required />
-							</div>
-							<div class="input">
-								<span class="fa fa-mail" aria-hidden="true"></span>
-								<input type="text" placeholder="Email" name="email" required />
-							</div>
-							<div class="input">
-								<span class="fa fa-key" aria-hidden="true"></span>
-								<input type="password" placeholder="Password" name="password" required />
-							</div>
-							<div class="input">
-								<span class="fa fa-key" aria-hidden="true"></span>
-								<input type="password" placeholder="Confirm Password" name="password" required />
-							</div>
-							<button type="submit" class="btn submit">Register</button>
-						</form>
-					</article>
-				</div>
-			</div>
-			<!-- //vertical tabs -->
-			<div class="clear"></div>
-	</div>
+    <h2 class="text-center">Sign in</h2>
+    
 
     <!-- /////////////////////////////// -->
     <!-- Page content ends -->
