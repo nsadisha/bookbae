2dd15f27315fd756c1fe57c923034c81cd020415
nsadisha <nsadisha@gmail.com>
2021-09-22 11:34:39 +0530

    add books to cart

---
+++ php/addToCart.php
@@ -0,0 +1,19 @@
+<?php include 'helper.php' ?>
+
+<?php
+
+//add an item to cart
+if(!isSigned() || !isset($_REQUEST["isbn"])){
+    goBack();
+}
+//get email
+$email = "example@example.com";
+$isbn = $_REQUEST["isbn"];
+$quantity = $_REQUEST["quantity"];
+
+$delete = execute("SELECT * FROM cart C WHERE EXISTS (SELECT * FROM cart WHERE email=\"$email\" AND isbn=\"$isbn\")");
+$delete = execute("INSERT INTO cart VALUES(\"$email\", \"$isbn\", \"$quantity\")");
+
+closeTab();
+
+?>
