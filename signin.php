17864fbc04cd82d570764be5319f81d5d16bff58
Vinojan Veerapathrathasan <48822560+Vinojan1999@users.noreply.github.com>
2021-09-22 20:01:44 +0530

    Account page updated
    Forms are divided into two pages
    Added necessary classes
    Reduced button font-size
    Removed ver-tab JS function

--- signin.php
+++ signin.php
@@ -10,8 +10,21 @@
     <!-- custom css -->
     <link rel="stylesheet" href="css/styles.css">
 	<link rel="stylesheet" href="css/signin.css">
-    <link rel="stylesheet" href="css/responsive.css">	
+    
+    <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
     <title>BookBae | Sign in</title>
+    <script src="js/signin.js"></script>
+
+    <script>
+        window.console = window.console || function(t) {};
+    </script>
+
+    <script>
+        if (document.location.search.match(/type=embed/gi)) {
+            window.parent.postMessage("resize", "*");
+    }
+    </script>
 </head>
 <body>
 
@@ -20,16 +33,47 @@
     <!-- Navbar ends -->
 
     <!-- Page content starts -->
-    <h2 class="text-center">Sign in</h2>
-    
+    <div class="form">
+        
+        
+        <div class="tab-content">
+        <div id="login">   
+            <h1>Welcome Back!</h1>
+            
+            <form action="/" method="post">
+            
+                <div class="field-wrap">
+                <label>
+                Email Address<span class="req">*</span>
+                </label>
+                <input type="email"required autocomplete="off"/>
+            </div>
+            
+            <div class="field-wrap">
+                <label>
+                Password<span class="req">*</span>
+                </label>
+                <input type="password"required autocomplete="off"/>
+            </div>
+            
+            <p class="forgot"><a href="#">Forgot Password?</a></p>
+            
+            <button class="button button-block"/>Log In</button>
+            
+            </form>
 
-    <!-- /////////////////////////////// -->
+            </div>
+            
+            <div>
+            </div>
+            
+        </div><!-- tab-content -->
+      
+    </div> <!-- /form -->
     <!-- Page content ends -->
 
     <!-- Footer starts -->
-    <div class="login-footer">
-		<?php footer(); ?>
-	</div>
+    <?php footer(); ?>
     <!-- Footer ends -->
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
