2485f57eb2a94021c22ea9ce49ef6122744aefdb
tchamalsha <tharu.chamalsha@gmail.com>
2021-09-25 09:46:46 +0530

    connect database

--- components/user.php
+++ components/user.php
@@ -1,6 +1,7 @@
 <?php
 
 $conn=mysqli_connect('localhost','root','','bookbae');
+
 if(isset($_REQUEST['submit'])){
     $fname=$_REQUEST['fname'];
     $lname=$_REQUEST['lname'];
@@ -8,6 +9,15 @@ if(isset($_REQUEST['submit'])){
     $contact=$_REQUEST['contact'];
     $password=$_REQUEST['password'];
 
+    $sql="Insert into users values(\"$email\",\"$fname\",\"$lname\",\"$contact\")";
+
+    $result=$conn->query($sql);
+    if($result)
+    {
+        echo 'success';
+    }else{
+        echo "error";
+    }
 
 }
 
