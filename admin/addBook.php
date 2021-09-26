abcfdc6eab9831f7feb543dd4e32be9df962c4fe
nsadisha <nsadisha@gmail.com>
2021-09-26 17:25:42 +0530

    little comments

--- admin/addBook.php
+++ admin/addBook.php
@@ -8,6 +8,7 @@ if(!isAdminSigned()){
 }
 
 if (isset($_REQUEST["submit"])) {
+    //request data
     $isbn = $_REQUEST["isbn"];
     $name = $_REQUEST["name"];
     $author = $_REQUEST["author"];
@@ -20,11 +21,13 @@ if (isset($_REQUEST["submit"])) {
     $quantity = $_REQUEST["quantity"];
     $description = $_REQUEST["description"];
 
+    //add data to the databse
     $result1 = execute("INSERT INTO books VALUES(\"$isbn\", \"$name\", \"$price\", \"$category\", \"$language\", \"$author\", \"$year\", \"$edition\", \"$publisher\", \"$quantity\")");
     var_dump($result1);
     //file uploading
     if($result1){
         uploadFile("image_1","$isbn-1", $isbn);
+        //if image 2 exists
         if(!empty($_FILES["image_2"]["name"])){
             uploadFile("image_2","$isbn-2", $isbn);
         }
@@ -35,12 +38,16 @@ header("Location: index.php");
 
 
 function uploadFile($file, $name, $isbn){
+    //targeted directory
     $target_dir = "../data/";
+    //getting the file extention
     $tmp = explode('.', $_FILES[$file]["name"]);
     $ext = end($tmp);
+    //targetted file path
     $target_file = $target_dir . $name.".".$ext;
     $uploadOk = 1;
     
+    //if file is already exists
     if(file_exists($target_file)) {
         echo "Sorry, file already exists.";
         $uploadOk = 0;
@@ -61,6 +68,8 @@ function uploadFile($file, $name, $isbn){
     } else {
         if (move_uploaded_file($_FILES[$file]["tmp_name"], $target_file)) {
             echo " The file ". htmlspecialchars( basename( $_FILES[$file]["name"])). " has been uploaded.<br>";
+
+            //add image name to the database table
             execute("INSERT INTO book_images VALUES(\"$isbn\", \"$name.$ext\")");
         } else {
             echo " Sorry, there was an error uploading your file.<br>";
