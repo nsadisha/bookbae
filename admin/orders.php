897633bcdd0e3532ab8c13303f3c51bcd794e3d7
nsadisha <nsadisha@gmail.com>
2021-09-26 13:14:20 +0530

    orders page done

--- admin/orders.php
+++ admin/orders.php
@@ -16,8 +16,106 @@ if(isset($_REQUEST["filter"])){
 // complete order
 if(isset($_REQUEST["complete"])){
     $id = $_REQUEST["id"];
-    $result = execute("UPDATE TABLE orders SET status='Shipped' WHERE order_id=\"$id\"");
-    var_dump($result);
+    $result = execute("UPDATE orders SET status='Shipped' WHERE order_id=\"$id\"");
+    header("Location: orders.php?filter=$filter");
+}
+
+
+//display all orders
+function displayAllOrders($status){
+    global $filter;
+    $orders = "";
+    if($filter=="All"){
+        $orders = execute("SELECT * FROM orders");
+    }else{
+        $orders = execute("SELECT * FROM orders WHERE status IN(\"$status\")");
+    }
+    if($orders->num_rows==0){
+        echo "<h2 class='text-center'>No $status orders!</h2>";
+    }
+    foreach($orders as $order){
+        $id = $order["order_id"];
+        $total = number_format($order["total_price"], 2);
+        $date = "On ".explode(" ", $order["date"])[0]." at ".explode(" ", $order["date"])[1];
+        $note = $order["note"];
+        $email= $order["email"];
+        $name = get("SELECT Concat(fname, ' ', lname) AS name FROM users WHERE email=\"$email\"")["name"];
+        $address = get("SELECT Concat(line1, ', ', line2, ',<br>', city, ',<br>', province, '<br>', zip) AS address FROM user_addresses WHERE email=\"$email\"")["address"];
+
+        $isDisabled = $order["status"]!="Processing"?"disabled":"";
+
+        // var_dump($order);
+        $orderItem = "
+            <div class='accordion-item'>
+                <h2 class='accordion-header' id='headingOne'>
+                <button class='accordion-button' type='button' data-bs-toggle='collapse' data-bs-target='#$id' aria-expanded='true' aria-controls='collapseOne'>
+                    <strong>Order #$id</strong>
+                </button>
+                </h2>
+                <div id='$id' class='accordion-collapse collapse' aria-labelledby='headingOne' data-bs-parent='#orders'>
+                    <div class='accordion-body'>
+                        <div class='row'>
+                            <div class='col-sm mb-3 mb-sm-0'>
+                                <h4 class='text-brown mb-1'><strong>Rs. $total</strong></h4>
+                                <span class='text-secondary'>$date</span>
+                                <h4 class='mt-3 mb-1'><strong>Order notes</strong></h4>
+                                <span class='text-secondary'>$note</span>
+                            </div>
+                            <div class='col-sm-4'>
+                                <h4 class='mb-1'><strong>Address</strong></h4>
+                                <span class='text-secondary'>$name<br>$address</span>
+                            </div>
+                        </div>
+                        <h4 class='mt-3'><strong>Items</strong></h4>
+                        <div class='order-item-list table-responsive'>
+                            <table class='table'>
+                                <thead>
+                                    <tr>
+                                    <th scope='col'>NAME</th>
+                                    <th scope='col'>ISBN</th>
+                                    <th scope='col'>PRICE</th>
+                                    <th scope='col'>QTY</th>
+                                    <th scope='col'>TOTAL</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    ".showOrderItems($id)."
+                                </tbody>
+                            </table>
+                            <div class='mt-3'>
+                                <form action='".$_SERVER["PHP_SELF"]."' method='get' class='d-flex'>
+                                    <input type='hidden' name='id' value='$id'>
+                                    <input type='hidden' name='filter' value='$status'>
+                                    <button type='submit' class='btn bg-brown text-white ms-auto' name='complete' $isDisabled><strong>Complete order</strong></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ";
+
+        echo $orderItem;
+    }
+}
+
+function showOrderItems($id){
+    $items = execute("SELECT B.name, B.isbn, B.price, O.quantity, O.quantity * B.price 'total' FROM order_items O RIGHT OUTER JOIN books B ON O.isbn=B.isbn WHERE O.order_id=\"$id\"");
+    $itemList = "";
+    foreach($items as $item){
+        $tableItem = "
+            <tr>
+            <td>".$item["name"]."</td>
+            <td>".$item["isbn"]."</td>
+            <td>Rs. ".number_format($item["price"], 1)."</td>
+            <td>".$item["quantity"]."</td>
+            <td>Rs. ".number_format($item["total"], 2)."</td>
+            </tr>
+        ";
+        
+        $itemList = $itemList.$tableItem;
+    }
+    return $itemList;
 }
 ?>
 
@@ -86,61 +184,17 @@ if(isset($_REQUEST["complete"])){
 
         <div class="row">
             <div class="accordion" id="orders">
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingOne">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        <strong>Order #were165535s63c</strong>
-                    </button>
-                    </h2>
-                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#orders">
-                        <div class="accordion-body">
-                            <h3 class="text-brown mb-1">Rs. 2,123.50</h3>
-                            <span class="text-secondary">On 2021-09-25 at 10:10:10</span>
-                            <h4 class="mt-3">Items</h4>
-                            <div class="order-item-list table-responsive">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                        <th scope="col">NAME</th>
-                                        <th scope="col">ISBN</th>
-                                        <th scope="col">PRICE</th>
-                                        <th scope="col">QTY</th>
-                                        <th scope="col">TOTAL</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>Sadisha Nimsara</td>
-                                            <td>Sadisha Nimsara</td>
-                                            <td>Sadisha Nimsara</td>
-                                            <td>Sadisha Nimsara</td>
-                                            <td>Sadisha Nimsara</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Sadisha Nimsara</td>
-                                            <td>Sadisha Nimsara</td>
-                                            <td>Sadisha Nimsara</td>
-                                            <td>Sadisha Nimsara</td>
-                                            <td>Sadisha Nimsara</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                                <div class="mt-3">
-                                    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="get" class="d-flex">
-                                        <input type="hidden" name="id" value="1231233214">
-                                        <input type="hidden" name="filter" value="<?php echo $filter; ?>">
-                                        <button type="submit" class="btn bg-brown text-white ms-auto" name="complete"><strong>Complete order</strong></button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php displayAllOrders($filter); ?>
             </div>
         </div>
     </section>
     
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    // make the first order exmanded
+    // var firstOrder = document.querySelector("accordion-collapse");
+    // firstOrder.classList.add("show");
+</script>
 </body>
 </html>
