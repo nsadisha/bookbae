2a455766c7fd8a6855ae63df7e646ba9b09d8dfe
nsadisha <nsadisha@gmail.com>
2021-09-28 04:04:20 +0530

    send mail with PHPMailer

---
+++ sendmail/sendMail.php
@@ -0,0 +1,62 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+// require('PHPMailer.php');
+// require('SMTP.php');
+// require('Exception.php');
+
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\SMTP;
+// use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'PHPMailerAutoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer();
+
+function sendMail($receiver, $subject, $body){
+    global $mail;
+    try {
+        //Server settings
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'lbookbae@gmail.com';                     //SMTP username
+        $mail->Password   = 'bookbae1234';                               //SMTP password
+        // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->SMTPSecure = "tls";            //Enable implicit TLS encryption
+        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom('lbookbae@gmail.com', 'BookBae');
+        $mail->addAddress($receiver);     //Add a recipient
+        // $mail->addAddress('ellen@example.com');               //Name is optional
+        // $mail->addReplyTo('info@example.com', 'Information');
+        // $mail->addCC('cc@example.com');
+        // $mail->addBCC('bcc@example.com');
+
+        //Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = $subject;
+        $mail->Body    = $body;
+        $mail->AltBody = 'Please use a HTML support application!';
+
+        if($mail->send()){
+            echo 'Message has been sent';
+        }else{
+            echo 'Message has not been sent';
+        }
+        $mail->smtpClose();
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+
+sendMail("gnsadisha@gmail.com", "This is a subject", "This is the <b>body</b>.<hr>");
+?>
