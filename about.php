4df0a8096aa9256f9c69c13f4c31ec5b70f2aba6
nsadisha <nsadisha@gmail.com>
2021-09-28 18:58:30 +0530

    about page

--- about.php
+++ about.php
@@ -1,4 +1,5 @@
 <?php include "components/footer.php" ?>
+<?php include "php/helper.php" ?>
 <?php include "components/navbar.php" ?>
 
 <!DOCTYPE html>
@@ -18,7 +19,21 @@
     <!-- Navbar ends -->
 
     <!-- Page content starts -->
-    <h1 class="text-center">About us</h1>
+    <section class="container mt-3">
+        <h2><strong>About us</strong></h2>
+        <div class="hr mb-3"></div>
+        <p>
+            As an online  store, leading in Books. The Book Bae has established itself as a reliable destination. Our range of books encompasses every aspect of a authenticity and quality, as we work towards bringing value to our customers and communities around the Island while our customers satisfaction is our top priority.<br><br>
+
+            At The Book Bae, through our books we try to improves lives and make everyone's lives more cool and even stylish. The books we are selling are suitable for any ages, making The Book Bae the best place to buy your every kind of books and our books go hand in hand with personal and caring 24/7 customer service.<br><br>
+
+            Every team member of our team is forward-thinking individuals who are motivated to give you a better and a lasting shopping experience. We are here to define and turn your shopping into a seamless and inspiring experience. We believe in equality for all. We believe in putting you first.<br><br>
+
+            The Book bae differentiate itself from its competitors as one of the most preferred online Book Store, within Sri Lanka and beyond by the service it provides.<br><br>
+
+            At The Book Bae, we believe you deserve the best product for the price you pay and that is why we are devoted to make it happen. Shop with us today and be part of our great family and learn the true value of CUSTOMER CARE
+        </p>
+    </section>
     <!-- Page content ends -->
 
     <!-- Footer starts -->
