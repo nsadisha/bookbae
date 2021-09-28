e25fdd8c67f23d736227237b61984c8f80ead4d9
nsadisha <nsadisha@gmail.com>
2021-09-28 19:16:53 +0530

    covid 19 update

---
+++ covid19-update.php
@@ -0,0 +1,47 @@
+<?php include "components/footer.php" ?>
+<?php include "php/helper.php" ?>
+<?php include "components/navbar.php" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- custom css -->
+    <link rel="stylesheet" href="css/styles.css">
+    <title>BookBae | Covid 19 Update</title>
+</head>
+<body>
+
+    <!-- Navbar starts -->
+    <?php navbar('contact'); ?>
+    <!-- Navbar ends -->
+
+    <!-- Page content starts -->
+    <section class="container mt-3">
+        <h2><strong>Covid 19 Update</strong></h2>
+        <div class="hr mb-3"></div>
+        <p>
+        Dear Customers!<br><br>
+
+        We just want you to know that we are taking every precaution in creating a safe atmosphere for our staff and customers!<br><br>
+
+        The health and safety continues to be our top priority as we face the challenges associated with COVID-19. Prior to being shipped, each product, package, paper, sticker, etc. is sterilized to ensure safety for our customers. One of the many benefits of supporting small businesses is that we are able to inspect every single package that is shipped out to you!<br><br>
+
+        Please notice that our carriers and shipping companies are experiencing delivery delays due to the sterilization process, but everyone is doing their best to deliver your package as soon as possible.<br><br>
+        We will ALWAYS be here to answer your questions, track your order and make sure it is delivered safe and sound to your doorstep!<br><br>
+        From our family to yours, stay safe and keep your heads up!<br><br>
+
+        <strong>Please contact us anytime at:</strong> <a href="mailto:contact@bookbae.store">contact@bookbae.store</a>
+        </p>
+    </section>
+    <!-- Page content ends -->
+
+    <!-- Footer starts -->
+    <?php footer(); ?>
+    <!-- Footer ends -->
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
